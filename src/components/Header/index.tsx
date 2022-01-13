bd06b51cddc5ce9c3c63240d0f045173e8610341
ArturRibeiro01 <profissional.arturrc@gmail.com>
2022-01-12 21:21:36 -0300

    react-modal installation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg";
 import * as S from "./styles";
 
 export const Header = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <S.Container>
       <S.Content>
         <img src={logoImg} alt="dt-mony" />
-        <button type="button">Nova transação</button>
+        <button onClick={handleOpenNewTransactionModal} type="button">
+          Nova transação
+        </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </S.Content>
     </S.Container>
   );
